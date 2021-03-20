d939d18de05f7e77b15be813cadfd91399576150
waterlimone <cmcilvenna99@gmail.com>
2021-03-20 18:29:22 -0400

    Removed implemention from parser.h and moved implementation over to parser.cpp.

---
+++ parser.cpp
@@ -0,0 +1,53 @@
+//////////////////////////////////////////////////////////////
+// by: Cameron McIlvenna                                    //
+// date: 1/22/21                                            //
+// purpose: Fasta Parser Package Header                     //
+//////////////////////////////////////////////////////////////
+
+#pragma
+#include <fstream>
+#include <string>
+#include <vector>
+#include "parser.h"
+namespace parse {
+
+  int numLines(std::string fileName){
+    std::ifstream inFile(fileName);
+    std::string myLine;
+    std::string tempStr;
+    int numGreater = 0;
+
+    while(getline(inFile, myLine)){
+      tempStr = myLine[0];
+
+      if(tempStr.compare(">") == 0){
+        numGreater ++;
+      }
+    }
+    inFile.close();
+    return numGreater;
+  }
+
+  pFast fasta(std::string fileName){
+    pFast melon;
+    std::ifstream inFile(fileName);
+    int arraySize = numLines(fileName);
+    int index = 0;
+    std::string myLine;
+    std::string tempStr = "";
+    melon.numFasta = arraySize;
+
+    while(getline(inFile, myLine)){
+      tempStr = myLine[0];
+
+      if(tempStr.compare(">") == 0){
+        melon.head.push_back(myLine);
+      }else{
+              melon.body.push_back(myLine);
+           }
+    }
+    inFile.close();
+    return melon;
+  }
+
+}
--- parser.h
+++ parser.h
@@ -4,29 +4,12 @@
 // purpose: Fasta Parser Package Header                     //
 //////////////////////////////////////////////////////////////
 
-#pragma
-#include <fstream>
-#include <string>
-#include <vector>
+#include<string>
+#include<vector>
 
 namespace parse {
 
-  int numLines(std::string fileName){
-    std::ifstream inFile(fileName);
-    std::string myLine;
-    std::string tempStr;
-    int numGreater = 0;
-
-    while(getline(inFile, myLine)){
-      tempStr = myLine[0];
-
-      if(tempStr.compare(">") == 0){
-        numGreater ++;
-      }
-    }
-    inFile.close();
-    return numGreater;
-  }
+  int numLines(std::string fileName);
 
   struct pFast{
     std::vector <std::string> head;
@@ -35,26 +18,5 @@ namespace parse {
   };
 
 
-  pFast fasta(std::string fileName){
-    pFast melon;
-    std::ifstream inFile(fileName);
-    int arraySize = numLines(fileName);
-    int index = 0;
-    std::string myLine;
-    std::string tempStr = "";
-    melon.numFasta = arraySize;
-
-    while(getline(inFile, myLine)){
-      tempStr = myLine[0];
-
-      if(tempStr.compare(">") == 0){
-        melon.head.push_back(myLine);
-      }else{
-              melon.body.push_back(myLine);
-           }
-    }
-    inFile.close();
-    return melon;
-  }
-
+  pFast fasta(std::string fileName);
 }
