6bad7d7824819fd70808eac7b684d2cfb876fce2
waterlimone <cmcilvenna99@gmail.com>
2021-03-21 15:55:26 -0400

    Cleaned up code base

--- compile.sh
+++ compile.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-g++ -o run.exe main.cpp
+g++ -o run.exe main.cpp parser.cpp
--- main.cpp
+++ main.cpp
@@ -19,19 +19,19 @@ std::vector<gene> genes;
 void loadGenes(std::string fileName){
   std::vector<std::string>::iterator head; //creates iterators for For Loop
   std::vector<std::string>::iterator body;
-  pFast melon = fasta(fileName); // melon passes back strussct to be be fed to the class (gene)
+  pFast melon = fasta(fileName); // melon passes back structs to be be fed to the class (gene)
   std::string headers[melon.numFasta]; //creates two arrays of correct size to be passed to the class (gene)
   std::string bodies[melon.numFasta];  //continued...
 
 // Creates an array of gene (name of the class)
-    for(head = melon.head.begin(), body = melon.body.begin(); head < melon.head.end(), body < melon.body.end(); head++, body++){ //uses the iterator begin() to iterator end() to traverse the vector//
+    for(head = melon.head.begin(), body = melon.body.begin(); body < melon.body.end(); head++, body++){ //uses the iterator begin() to iterator end() to traverse the vector//
         genes.push_back(gene(*head, *body));
+        std::cout << *head << "\n";
     }
 }
 
 int main(){
   loadGenes("Data_RNA.fasta");
-
   return 0; // returns 0 *why are u here lol* //
 }
 
